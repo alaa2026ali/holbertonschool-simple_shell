73663bff06b1f26ad27b3a88006be84fdf6fccd9
alaa2026ali <ali_alaa18@icloud.com>
2026-05-11 09:49:46 +0300

    Add execute_cmd function declaration in main.h

--- main.h
+++ main.h
@@ -4,10 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
 extern char **environ;
+
+void execute_cmd(char *command);
 
 #endif
