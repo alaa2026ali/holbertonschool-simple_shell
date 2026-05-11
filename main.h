db42c25f5f7455eacfdaca65f961a8c4641e0c52
alaa2026ali <ali_alaa18@icloud.com>
2026-05-11 09:52:46 +0300

    Add function prototype and environment variable access

--- main.h
+++ main.h
@@ -1,14 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Standard libraries */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
+/* Access to environment variables */
 extern char **environ;
 
+/* Function prototype */
 void execute_cmd(char *command);
 
-#endif
+#endif /* MAIN_H */
