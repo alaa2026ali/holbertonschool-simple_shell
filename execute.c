d456d39b37492c2ed71e29eec58767f747ad1794
alaa2026ali <ali_alaa18@icloud.com>
2026-05-13 10:09:21 +0300

    Refactor execute_cmd to accept command string
    Refactor execute_cmd to tokenize command string and handle execution.

--- execute.c
+++ execute.c
@@ -1,28 +1,39 @@
 #include "main.h"
 
 /**
- * execute_cmd - Executes a command
- * @argv: arguments array
+ * execute_cmd - Tokenizes a string into arguments and executes the binary
+ * @command: Raw input string containing the command line
  */
-void execute_cmd(char **argv)
+void execute_cmd(char *command)
 {
 	pid_t pid;
 	int status;
+	char *argv[32];
+	char *token;
+	int i = 0;
+
+	token = strtok(command, " \t\r\n\a");
+	while (token != NULL && i < 31)
+	{
+		argv[i] = token;
+		i++;
+		token = strtok(NULL, " \t\r\n\a");
+	}
+	argv[i] = NULL;
+
+	if (argv[0] == NULL)
+		return;
 
 	pid = fork();
-
 	if (pid == -1)
-	{
-		perror("Error");
 		return;
-	}
 
 	if (pid == 0)
 	{
 		if (execve(argv[0], argv, environ) == -1)
 		{
 			perror("./hsh");
-			exit(1);
+			exit(127);
 		}
 	}
 	else
