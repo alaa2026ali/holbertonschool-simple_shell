922e4e844f8ae17704b574b23380b046e2e4ec30
alaa2026ali <ali_alaa18@icloud.com>
2026-05-13 09:58:37 +0300

    Refactor execute_cmd to use argument array
    Refactored execute_cmd to accept an array of arguments instead of a raw command string. Removed tokenization logic and streamlined the execution process.

--- execute.c
+++ execute.c
@@ -1,45 +1,24 @@
 #include "main.h"
 
 /**
- * execute_cmd - Tokenizes and executes a command using fork and execve
- * @command: Raw input string containing the command
+ * execute_cmd - executes a command
+ * @argv: array of arguments
  */
-void execute_cmd(char *command)
+void execute_cmd(char **argv)
 {
 	pid_t pid;
 	int status;
-	char *argv[32]; /* Safe array size to hold commands and arguments */
-	char *token;
-	int i = 0;
-
-	/* Strip trailing/leading spaces, tabs, and newlines */
-	token = strtok(command, " \t\r\n\a");
-	while (token != NULL && i < 31)
-	{
-		argv[i] = token;
-		i++;
-		token = strtok(NULL, " \t\r\n\a");
-	}
-	argv[i] = NULL;
-
-	/* If input was empty or only spaces, do nothing */
-	if (argv[0] == NULL)
-		return;
 
 	pid = fork();
+
 	if (pid == -1)
 		return;
 
 	if (pid == 0)
 	{
-		if (execve(argv[0], argv, environ) == -1)
-		{
-			/* Safe exit to allow the main loop to continue parsing */
-			exit(127);
-		}
+		execve(argv[0], argv, environ);
+		exit(1);
 	}
 	else
-	{
 		wait(&status);
-	}
 }
